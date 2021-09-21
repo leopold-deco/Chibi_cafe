665e0d4e56a30a21eddf1cd11b1a306eca3a9869
leopold-deco <leopold.deconinck@gmail.com>
2021-09-21 17:22:51 +0200

    Mise en place de l'architecture

--- chibi-api/index.js
+++ chibi-api/index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const router = require('./app/router');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);

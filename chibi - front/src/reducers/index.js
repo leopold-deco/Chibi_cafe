56007f385afa78135f5f60f74d35747313e7f94e
Claudie-code <65274376+Claudie-code@users.noreply.github.com>
2021-09-22 16:16:40 +0200

    reducer user done

--- chibi - front/src/reducers/index.js
+++ chibi - front/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import productsReducer from './products';
+import userReducer from './user';
 
 const rootReducer = combineReducers({
   products: productsReducer,
+  user: userReducer,
 });
 
 export default rootReducer;

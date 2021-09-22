e656644fa19bc9bdafc919512da08ff4c660150e
RichardBefayt <sprichie.rb@gmail.com>
2021-09-22 17:24:42 +0200

    contact done

--- chibi - front/src/components/Contact/Form/index.js
+++ chibi - front/src/components/Contact/Form/index.js
@@ -5,23 +5,23 @@ const Form = () => (
             <h2 className="form__title">Contactez-nous</h2>
 
             <div className="form__lastname">
-                <label>Nom :</label>
-                <input className="form__input" type="text" />
+                <label className="form__lastname-label">Nom</label>
+                <input className="form__lastname-input" type="text" />
             </div>
             
             <div className="form__firstname">
-                <label>Prénom :</label>
-                <input className="form__input" type="text" />
+                <label className="form__firstname-label">Prénom</label>
+                <input className="form__firstname-input" type="text" />
             </div>
             
             <div className="form__email">
-                <label>Email :</label>
-                <input className="form__input" type="email" />
+                <label className="form__email-label">Email</label>
+                <input className="form__email-input" type="email" />
             </div>
             
             <div className="form__phone">
-                <label>Téléphone :</label>
-                <input className="form__input" type="number" />
+                <label className="form__phone-label">Téléphone</label>
+                <input className="form__phone-input" type="number" />
             </div>
     </div>
 );
--- chibi - front/src/components/Contact/Message/index.js
+++ chibi - front/src/components/Contact/Message/index.js
@@ -3,7 +3,7 @@ import './message.scss';
 const Message = () => (
     <div className='message'>
         <label className="message__label">Message</label>
-        <textarea className="message__text" placeholder="Votre Message"></textarea>
+        <textarea className="message__text" rows="20" cols="75" placeholder="Votre Message"></textarea>
 
         <button className="message__button" type="submit">Envoyer</button>
     </div>

d5ae1756abbbf99ac702ef6f1d06c05d8fb37d92
mathieu <mathieu.figari@gmail.com>
2021-09-23 09:58:02 +0200

    pull front

--- chibi - front/src/reducers/index.js
+++ chibi - front/src/reducers/index.js
@@ -1,11 +1,7 @@
 import { combineReducers } from 'redux';
-
+import  userReducer  from "./user"
 import productsReducer from './products';
-<<<<<<< HEAD
 import shop from './shopReducer'
-=======
-import userReducer from './user';
->>>>>>> bd8f4e22a9fe9281536b603919af1872ec8eaa0d
 
 const rootReducer = combineReducers({
   shop,

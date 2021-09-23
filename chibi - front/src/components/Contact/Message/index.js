872d24e1d6dad97fb249d3ed96b726a14c55b9a6
RichardBefayt <sprichie.rb@gmail.com>
2021-09-23 09:05:24 +0200

    merge front

--- chibi - front/src/components/Contact/Message/index.js
+++ chibi - front/src/components/Contact/Message/index.js
@@ -1,12 +1,14 @@
 import './message.scss';
 
-const Message = () => (
+const Message = () => { 
+
+    return (
     <div className='message'>
         <label className="message__label">Message</label>
         <textarea className="message__text" rows="20" cols="75" placeholder="Votre Message"></textarea>
 
         <button className="message__button" type="submit">Envoyer</button>
     </div>
-);
+);}
 
 export default Message;

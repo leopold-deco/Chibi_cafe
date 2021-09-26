875cab82c2715991d52d3365ed43e9786b4688d5
mathieu <mathieu.figari@gmail.com>
2021-09-26 12:38:44 +0200

    header integration ok

--- chibi - front/src/components/AppHeader/NavBar/index.js
+++ chibi - front/src/components/AppHeader/NavBar/index.js
@@ -1,12 +1,24 @@
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { useState } from 'react';
 import './navbar.scss';
 
 
 function NavBar({cartCount, state}) {
+const [open, setMenu] = useState(false)
 
+const className = open ? "menu-btn open" : "menu-btn"
+
+const openBurger = () => {
+  if(open === true){
+    setMenu(false)
+  } else {
+    setMenu(true)
+  }
+}
 
     return (
+      <> 
       <div className="navbar">
         <div className="navbar__1">
           <NavLink className="navlink" activeClassName="isactive" exact to="/">Accueil</NavLink>
@@ -19,6 +31,10 @@ function NavBar({cartCount, state}) {
           <NavLink className="navlink"  activeClassName="isactive" exact to="/contact">Contact</NavLink>
         </div>
       </div>
+      <div onClick={openBurger} class={className}>
+      <div class="menu-btn__burger"></div>
+      </div>
+    </>
     );
   };
 

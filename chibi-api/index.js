1d28e94a48cd4230daeb8d685fc460a58a3319f6
leopold-deco <leopold.deconinck@gmail.com>
2021-09-27 11:55:18 +0200

    essaie2 sur le corsOption

--- chibi-api/index.js
+++ chibi-api/index.js
@@ -9,14 +9,14 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-const corsOptions ={
-    origin:'https://chibi-cafe.netlify.app',
-    credentials:true,           
-    optionSuccessStatus:200
-}
+// const corsOptions ={
+//     origin:'https://chibi-cafe.netlify.app',
+//     credentials:true,
+//     optionSuccessStatus:200
+// }
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json())
 app.use(router);
 

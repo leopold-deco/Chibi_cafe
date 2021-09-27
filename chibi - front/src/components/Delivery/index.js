494193a9d8e37d68a78c8470a5fb70caffbc6a2f
Claudie-code <65274376+Claudie-code@users.noreply.github.com>
2021-09-27 09:09:43 +0200

    localstorage delivery

--- chibi - front/src/components/Delivery/index.js
+++ chibi - front/src/components/Delivery/index.js
@@ -9,31 +9,43 @@ import NewAddress from './NewAddress';
 
 const Delivery = () => {
     const dispatch = useDispatch();
-    const { 
-        isNewAddress
-    } = useSelector((state) => state.delivery);
+    const { isNewAddress } = useSelector((state) => state.delivery);
+    const delivery = useSelector((state) => state.delivery);
+    const user = useSelector((state) => state.user);
 
     const handleChange = (value, name) => {
         dispatch(setDeliveryField(value, name));
     };
+
+    const verifyIsNewAddressAndStore = () => {
+        console.log('test')
+        if(isNewAddress) {
+            console.log("nex")
+            localStorage.setItem("delivery", delivery)
+        } else {
+            localStorage.setItem("delivery", user)
+        }
+        console.log("deliv",localStorage.getItem("delivery"))
+    };
+
     return (
-        <Form handleSubmit="test"> 
+        <Form handleSubmit={verifyIsNewAddressAndStore}> 
             <h2>Livraison</h2>
             <div className="delivery">
                 <div className="delivery__elements">
                     <Input type="radio" name="isNewAddress" id="userAddress"
-                            value="true"
+                            value="false"
                             handleChange={handleChange}
-                            checked={isNewAddress === true}
+                            checked={isNewAddress === false}
                         />
                     <label htmlFor="userAddress">Livraison à votre adresse</label>
                     <UserAddress />
                 </div>
                 <div className="delivery__elements">
                     <Input type="radio" name="isNewAddress" id="newAddress"
-                            value="false"
+                            value="true"
                             handleChange={handleChange}
-                            checked={isNewAddress === false}
+                            checked={isNewAddress === true}
                         />
                     <label htmlFor="newAddress">Livraison à une autre adresse</label>
                     <NewAddress />

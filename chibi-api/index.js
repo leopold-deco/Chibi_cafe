a69522e4b8b905b67f10605f815062786e07a123
leopold-deco <leopold.deconinck@gmail.com>
2021-09-27 11:09:29 +0200

    modif url front chibi-cafe

--- chibi-api/index.js
+++ chibi-api/index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions ={
-    origin:'https://chibi-cafe.netlify.app/',
+    origin:'https://chibi-cafe.netlify.app',
     credentials:true,           
     optionSuccessStatus:200
 }

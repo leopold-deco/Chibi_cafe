38d78e0545eb0f2d4c97e2541d846a9b22d58a82
mathieu <mathieu.figari@gmail.com>
2021-09-27 10:32:50 +0200

    fixed conflicts

--- chibi - front/src/reducers/index.js
+++ chibi - front/src/reducers/index.js
@@ -2,12 +2,7 @@ import { combineReducers } from 'redux';
 import  userReducer  from "./user"
 import productsReducer from './products';
 import shop from './shopReducer'
-<<<<<<< HEAD
-
-=======
-import userReducer from './user';
 import deliveryReducer from './delivery';
->>>>>>> 166e09f23589ddf0c524fef5651a81740477a539
 
 const rootReducer = combineReducers({
   shop,

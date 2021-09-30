c04057924a5b301876599f5f055f60277caca20e
leopold-deco <leopold.deconinck@gmail.com>
2021-09-30 11:47:31 +0200

    checkPrice -OK-

--- chibi-api/app/function/checkPrice.js
+++ chibi-api/app/function/checkPrice.js
@@ -1,7 +1,6 @@
 const Product = require('../models/Product');
 
 const checkPrice = async (cart) => {
-    console.log('Cart dans checkprice', cart);
     const tabCart = [];
         for(const item of cart){
             const quantity = item.quantity;

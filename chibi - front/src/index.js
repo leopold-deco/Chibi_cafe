d90e9d636917030603b9bd279f1997028b136772
Claudie-code <65274376+Claudie-code@users.noreply.github.com>
2021-10-01 11:07:46 +0200

    fix redirect

--- chibi - front/src/index.js
+++ chibi - front/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/App';
 import store from './store';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter  as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(

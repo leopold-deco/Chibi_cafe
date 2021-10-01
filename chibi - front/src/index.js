5a781e1dc2b046c422a3dde5ca40fa7e006dd323
Claudie-code <65274376+Claudie-code@users.noreply.github.com>
2021-10-01 11:04:36 +0200

    fix hashrouter

--- chibi - front/src/index.js
+++ chibi - front/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/App';
 import store from './store';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter  as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(

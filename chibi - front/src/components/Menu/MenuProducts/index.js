3c2946819ad8b12604b8ce39a53f306b2402b564
mathieu <mathieu.figari@gmail.com>
2021-10-03 18:24:14 +0200

    menu page responsive started

--- chibi - front/src/components/Menu/MenuProducts/index.js
+++ chibi - front/src/components/Menu/MenuProducts/index.js
@@ -9,10 +9,13 @@ const MenuProducts = ({ products, closeMenuProducts, menuClassName, classMenu, c
 
    return (
     <div className={`products-menu ${classMenu}`}>
-      <i onClick={closeMenuProducts} className="fas fa-times-circle"></i>              
+      <i onClick={closeMenuProducts} className="fas fa-times-circle"></i>
+      <div className="products-menu__contain">            
     { products.map((product) => (
         <ProductsItems key={product.id} {...product} />
+          
     ))}
+    </div> 
     <div className={`products-menu__cover ${menuClassName}`}>
         <figure className="products-menu__cover__front">
         <h1 className="products-menu__cover__front__title">Chibi Café</h1>
--- chibi - front/src/components/Store/ProductsPage/ProductCard.js
+++ chibi - front/src/components/Store/ProductsPage/ProductCard.js
@@ -1,17 +1,26 @@
-import { useDispatch } from 'react-redux';
-import { addProductToCart } from '../../../actions/shop';
+import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
+import { addProductToCart, addFavorites } from '../../../actions/shop';
 import PropTypes from 'prop-types';
 import { calculPrice } from '../../../pipes/calculPrice';
 
 const ProductCard = ({ product }) => {
   
-  const dispatch = useDispatch()
+  const cart = useSelector((state) => state.shop.cart)
 
-  const [quantityToCart, setQuantity] = useState(0)
+  const cardToCart = cart.find((card) => card.id === product.id )
+
+
+  const dispatch = useDispatch()
+  
 
   const onCart = () => {
-    dispatch(addProductToCart(product))
+    dispatch(addProductToCart(product));
+    
+  }
+
+  const onFavorites = () => {
+    dispatch(addFavorites(product));
   }
 
     return (
@@ -19,10 +28,19 @@ const ProductCard = ({ product }) => {
           <h3 className="storeCard__name">{product.product_name}</h3>
           <p className="storeCard__price">{calculPrice(Number(product.price_without_taxes), Number(product.taxe))} €</p>
           <button
+          id={product.id}
           className="storeCard__btn"
           onClick={onCart} 
           type="button"> ajouter au panier</button>
 
+          {
+            cardToCart ? <div>{cardToCart.quantity}</div> : ''
+          }
+          
+          <button
+          className="storeCard__btn"
+          onClick={onFavorites}
+          type="button"> Ajouter aux favoris</button>
       </div>
     );
   }

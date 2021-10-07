4ef83b2772b90ae259d1ba0da976b3e361376928
RichardBefayt <sprichie.rb@gmail.com>
2021-10-07 13:22:06 +0200

    image background ok

--- chibi - front/src/components/Store/ProductsPage/index.js
+++ chibi - front/src/components/Store/ProductsPage/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import imgBackgroundStore from "../../../assets/presentation-sables/background.jpg"
 
 import ProductCard from "./ProductCard";
 import { fetchArticles,} from "../../../actions/shop";
@@ -40,7 +42,7 @@ const ProductsPage = () => {
           {products.map((product) => (
             <ProductCard key={product.id} {...product} product={product} />
           ))}
-
+        <img className="store-container__img" src={imgBackgroundStore} alt="Image de fond boutique" />
       </div>
       </>
     );

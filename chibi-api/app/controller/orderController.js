01d6546bcf839d3c367cc97a1dc5d5d0f2c74101
leopold-deco <leopold.deconinck@gmail.com>
2021-10-08 15:52:10 +0200

    Modif du mail confirmation de commande_1.3

--- chibi-api/app/controller/orderController.js
+++ chibi-api/app/controller/orderController.js
@@ -67,11 +67,13 @@ const oderController = {
                     rejectUnauthorized: false
                 },
             });
+
+            const mailOrderDate = dayjs((createdOrder.order_date).locale('fr').format('DD MMMM YYYY'));
             var mailOptions = {
                 from: 'chibi.test3@gmail.com', 
                   to: userInfo.mail,
                  subject: `Récapitulatif de commande n° ${createdOrder.id} sur CHIBI`, 
-                  text: `Cet email tient lieu de confirmation pour votre paiement de ${deliveryInfo.total}, effectué le ${dayjs((createdOrder.order_date).locale('fr').format('DD MMMM YYYY'))} sur la boutique Chibi Cafe.
+                  text: `Cet email tient lieu de confirmation pour votre paiement de ${deliveryInfo.total}, effectué le ${mailOrderDate} sur la boutique Chibi Cafe.
                          La commande sera livré au ${deliveryInfo.street_number} ${deliveryInfo.name_of_the_road} ${deliveryInfo.postal_code} ${deliveryInfo.city}
                   Merci de votre commande ${deliveryInfo.first_name}`
                 };

f2e236d49a06c7dc9c48cce6015f9cc442162559
leopold-deco <leopold.deconinck@gmail.com>
2021-10-08 15:59:15 +0200

    Modif du mail confirmation de commande_1.5

--- chibi-api/app/function/checkPrice.js
+++ chibi-api/app/function/checkPrice.js
@@ -1,18 +1,18 @@
 const Product = require('../models/Product');
 
 const checkPrice = async (cart) => {
-    console.log('enter in checkprice');
+    // console.log('enter in checkprice');
     const tabCart = [];
         for(const item of cart){
             const quantity = item.quantity;
             const price = await Product.findOne(item.id);
             const amount = (quantity * (price.price_without_taxes * price.taxe).toFixed(2));
-            console.log('amout', amount);
+            // console.log('amout', amount);
             tabCart.push(amount);
-            console.log('tabCart ', tabCart);
+            // console.log('tabCart ', tabCart);
         }
         const cartAmount = tabCart.reduce((previousValue, currentValue) => previousValue + currentValue).toFixed(2);
-        console.log('cartAmoutLog : ', cartAmount);
+        // console.log('cartAmoutLog : ', cartAmount);
     return cartAmount;
 }
 

b3ee5bb5c44001c76fff4c703cf8b345779884b8
Claudie-code <65274376+Claudie-code@users.noreply.github.com>
2021-10-11 18:47:38 +0200

    accueil css ok

--- chibi - front/src/components/Carousels/index.js
+++ chibi - front/src/components/Carousels/index.js
@@ -1,8 +1,15 @@
 import './carousels.scss';
+import carousel1 from '../../assets/carrousel/carrousel1.jpg';
+import carousel2 from '../../assets/carrousel/carrousel2.jpg';
+import carousel3 from '../../assets/carrousel/carrousel3.jpg';
+import carousel4 from '../../assets/carrousel/carrousel4.jpg';
+import carousel5 from '../../assets/carrousel/carrousel5.jpg';
+import carousel6 from '../../assets/carrousel/carrousel6.jpg';
+import carousel7 from '../../assets/carrousel/carrousel7.jpg';
 
 export default function Carousels() {
     return (
-        <div style={{display: 'flex'}}>
+        <div className="carousels">
             <div className="slider">
                 <div className="slides">
                     <div className="slide">
@@ -12,33 +19,33 @@ export default function Carousels() {
                     <img src="https://cdn.pixabay.com/photo/2015/11/12/17/42/tea-1040677_960_720.jpg" alt="thé et infusion" />
                     </div>
                     <div className="slide">
-                    <img src="https://images.pexels.com/photos/2934200/pexels-photo-2934200.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="sablés" />
+                    <img src={carousel7} alt="sablés" />
                     </div>
                 </div>
             </div>
             <div className="slider">
                 <div className="slides">
                     <div className="slide">
-                        <img src="https://www.allbranded.fr/out/shop-fr/pictures/generated/product/1/480_480_80/93990_06.jpg" alt="café" />
+                        <img src={carousel1} alt="café" />
                     </div>
                     <div className="slide">
-                        <img src="https://cdn.pixabay.com/photo/2015/11/12/17/42/tea-1040677_960_720.jpg" alt="thé et infusion" />
+                        <img src={carousel2} />
                     </div>
                     <div className="slide">
-                    <img src="https://images.pexels.com/photos/2934200/pexels-photo-2934200.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="sablés" />
+                    <img src={carousel3} alt="sablés" />
                     </div>
                 </div>
             </div>
             <div className="slider">
                 <div className="slides">
                     <div className="slide">
-                    <img src="https://images.pexels.com/photos/290975/pexels-photo-290975.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="café" />
+                    <img src={carousel4} alt="café" />
                     </div>
                     <div className="slide">
-                    <img src="https://cdn.pixabay.com/photo/2015/11/12/17/42/tea-1040677_960_720.jpg" alt="thé et infusion" />
+                    <img src={carousel5} />
                     </div>
                     <div className="slide">
-                    <img src="https://images.pexels.com/photos/2934200/pexels-photo-2934200.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="sablés" />
+                    <img src={carousel6} alt="sablés" />
                     </div>
                 </div>
             </div>
--- chibi - front/src/components/Home/index.js
+++ chibi - front/src/components/Home/index.js
@@ -1,5 +1,6 @@
 import './home.scss';
 import Carousels from '../Carousels';
+import cafe from '../../assets/coffeeshop.jpg';
 
 function Home() {
   return (
@@ -20,14 +21,14 @@ function Home() {
         <Carousels />
       <div className="presentation">
       <div className="presentation__img">
-        <img src="https://cdn.stocksnap.io/img-thumbs/960w/tables-chairs_WUM7VBAPS8.jpg" alt="café" />
+        <img src={cafe} alt="café" />
         </div>
       
 
       <div className="home__presentation">
-        <p>CHIBI CAFE est un coffee shop / salon de thé situé en plein coeur de Paris.<br />
-          <br />
-          Nous vous proposons des cafés et thés de spécialité, des déclinaisons de pâtisseries et des boissons colorées.
+        <p>CHIBI CAFE est un coffee shop / salon de thé situé en plein coeur de Paris.</p>
+          
+        <p>  Nous vous proposons des cafés et thés de spécialité, des déclinaisons de pâtisseries et des boissons colorées.
           Une épicerie fine proposant un large choix de produits sucrés.</p>
       </div>
       </div>

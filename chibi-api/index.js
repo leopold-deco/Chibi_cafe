5dacb5fe23e8ab8ecb4f54f4b3d37b4372d0b094
leopold-deco <leopold.deconinck@gmail.com>
2021-10-11 13:20:17 +0200

    test Cors

--- chibi-api/index.js
+++ chibi-api/index.js
@@ -17,10 +17,10 @@ const port = process.env.PORT || 3000;
 // app.engine('handlebars', exphbs());
 // app.set('view engine', 'handlebars');
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({'Access-Control-Allow-Origin': '*',}));
 app.use(express.json());
 app.use('public', express.static('public'));
 app.use(express.static('./public'));
+app.use(cors({'Access-Control-Allow-Origin': '*',}));
 app.use(router);
 
 app.listen(port, () => {

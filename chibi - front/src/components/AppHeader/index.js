bf944b6fdf3d7fda46141ddc927c03c12f97f34d
Claudie-code <65274376+Claudie-code@users.noreply.github.com>
2021-10-11 19:34:20 +0200

    cursor + icon header ok

--- chibi - front/src/components/AppHeader/index.js
+++ chibi - front/src/components/AppHeader/index.js
@@ -1,11 +1,12 @@
 import './appheader.scss';
 import NavBar from "./NavBar";
+import panda from '../../assets/panda.png';
 
 function AppHeader() {
     return (
       <div className="header">
-          <h1 className="header__title">Chibi Café</h1>
-            <NavBar />
+          <h1 className="header__title">Chibi Café <img src={panda} alt='panda'></img></h1>
+          <NavBar />
       </div>
     );
   }

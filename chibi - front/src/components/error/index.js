0647fc8350d96c16258e45f095e7606eb3b2b137
Claudie-code <65274376+Claudie-code@users.noreply.github.com>
2021-10-12 11:27:24 +0200

    404 page css ok

--- chibi - front/src/components/error/index.js
+++ chibi - front/src/components/error/index.js
@@ -1,7 +1,12 @@
+import './error.scss';
+import CoffeeMachineLoader from '../CoffeeMachineLoader';
+
 function Error() {
     return (
-      <div >
-          404
+      <div className="error">
+          <h2>404</h2>
+          <CoffeeMachineLoader />
+          <p>Aller à <a className="error__button" href="/">l'accueil</a></p>
       </div>
     );
   }
